f719f8dd8fe2b91d65eafc5d88469f28aed94e35
Caden MacKenzie <cadenmackenzie@gmail.com>
2019-02-27 09:46:38 +0800

    classes objects and methods

---
+++ week_02/11_inheritance/02_dog.py
@@ -0,0 +1,9 @@
+'''
+
+Building on the dog class from the previous example, create a subclass of the dog class
+with at least one additional attribute. Also, override the sleep() and eat() methods to act
+slightly different.
+
+Create and object of this class and demonstrate it's functionality.
+
+'''
