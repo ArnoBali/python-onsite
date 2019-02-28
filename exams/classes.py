1397a5a1e295e3a70157293b9b257926f70d117f
martin-martin <breuss.martin@gmail.com>
2019-02-28 14:41:07 +0800

    add mini-exam 1

---
+++ exams/classes.py
@@ -0,0 +1,20 @@
+'''
+CLASSES AND OOP
+===============
+
+Define a python class that describes a book object.
+
+Every instance of the class should have:
+a title,
+an author,
+a total page number,
+and a current page number (where the reader is at)
+
+Also, define a method that allows the user to read the book for a certain number of pages
+Calling the method should update the current page number accordingly.
+
+Inform the user if they've reached the end of the book! :)
+
+Have fun, and no cheating ;)
+
+'''
