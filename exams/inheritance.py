8152cf6df99f1ea5a8dd6755a5c872b8294fe404
martin-martin <breuss.martin@gmail.com>
2019-03-04 07:29:57 +0800

    add inheritance exam

---
+++ exams/inheritance.py
@@ -0,0 +1,20 @@
+'''
+CLASSES AND INHERITANCE
+=======================
+
+1) Define an empty Movie class.
+
+2) Add a dunder init method that takes two arguments "year" and "title"
+
+3) Create a sub-class called "RomCom" that inherits from the Movie class
+
+4) Create another sub-class of the Movie class called "ActionMovie"
+    that overwrites the dunder init method of Movie and adds another
+    instance variable called "pg" that is set by default to the number 13.
+
+5) EXTRA: If you finish early, use the time to practice flushing out these
+    classes and white-boarding code. What attributes could a Movie class
+    contain? What methods? What tricks can you use through inheritance?
+    Any class attributes you could add?
+
+'''
