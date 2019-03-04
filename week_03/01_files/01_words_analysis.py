c98d1cf2e793ca6f470ca532280089299a1e37ce
martin-martin <breuss.martin@gmail.com>
2019-03-04 16:03:31 +0800

    buff up files labs

---
+++ week_03/01_files/01_words_analysis.py
@@ -0,0 +1,9 @@
+'''
+Write a script that reads in the words from the words.txt file and finds and prints:
+
+1. The shortest word (if there is a tie, print all)
+2. The longest word (if there is a tie, print all)
+3. The total number of words in the file.
+
+
+'''
---
+++ week_03/01_files/02_writing.py
@@ -0,0 +1,4 @@
+'''
+Write a script that reads in the contents of words.txt and writes the contents in reverse
+to a new file words_reverse.txt.
+'''
