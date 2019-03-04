0fa41f52bff579ff993ec379e059ce8aff727ffd
martin-martin <breuss.martin@gmail.com>
2019-03-04 16:28:53 +0800

    delete obsolete files

--- week_03/02_exception_handling/03_02_file_errs.py
+++
@@ -1,8 +0,0 @@
-'''
-Read in the first number from 'integers.txt' and perform a calculation
-with it.
-Make sure to catch at least two possible Exceptions (IOError and ValueError)
-with specific except statements, and continue to do the calculation
-only if neither of them applies.
-
-'''
