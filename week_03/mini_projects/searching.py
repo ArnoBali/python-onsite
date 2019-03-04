141c973ab2d3838090b3effafc8861bcd07a2e5b
martin-martin <breuss.martin@gmail.com>
2019-03-04 16:04:15 +0800

    add mini-project

---
+++ week_03/mini_projects/searching.py
@@ -0,0 +1,11 @@
+'''
+Write a script that searches a folder (and all its subfolders) on your computer and compiles a list of all of
+all .jpg files (with the complete path of the files).
+
+If you are feeling bold, create a list for each kind of file extension you find in the folder.
+
+Start with a small folder to make it easy to check if your program is working correctly. Then sub out the
+folder name with a bigger folder. This program should work for any specified folder on your computer.
+
+
+'''
