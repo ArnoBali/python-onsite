7826191b60b8c9cacbd0dcd20d7aa79364abbc8e
martin-martin <breuss.martin@gmail.com>
2019-03-04 16:26:55 +0800

    add inheritance exam solutions

--- exams/inheritance.py
+++ exams/inheritance.py
@@ -18,3 +18,22 @@ CLASSES AND INHERITANCE
     Any class attributes you could add?
 
 '''
+
+
+class Movie():
+
+    def __init__(self, year, title):
+        self.year = year
+        self.title = title
+
+
+class RomCom(Movie):
+    pass
+
+
+class ActionMovie(Movie):
+
+    def __init__(self, year, title, pg=13):
+        super().__init__(year, title)
+        self.pg = pg
+            
