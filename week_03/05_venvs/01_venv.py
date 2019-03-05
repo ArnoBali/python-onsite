b07f15898ef5a2b9daab5ee1370f4f1246cef274
martin-martin <breuss.martin@gmail.com>
2019-03-06 07:41:13 +0800

    add venv and pckgs labs

---
+++ week_03/05_venvs/01_venv.py
@@ -0,0 +1,13 @@
+'''
+In your CodingNomads folder create a new folder. Inside of that folder:
+
+1. Create a new virtual environment
+2. Activate the virtual environment
+3. Install at least 3 packages in the virtual environment.
+4. Freeze the installed packages to a requirements.txt file.
+5. Deactivate the virtual environment.
+6. Delete the virtual environment.
+7. Create a new virtual environment and install the packages from the requirements.txt file.
+
+
+'''
