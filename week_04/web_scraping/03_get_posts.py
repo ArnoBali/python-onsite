0d53c30edca8e76627ecc9947236ede1073585d4
martin-martin <breuss.martin@gmail.com>
2019-03-14 01:11:27 +0800

    add scraping labs

---
+++ week_04/web_scraping/03_get_posts.py
@@ -0,0 +1,15 @@
+'''
+Create an account at freecycle or use the following:
+user: martin-martin
+pwd:  bali2019
+
+Using python's request_html library:
+* log in to the website
+* navigate to the site that contains all posts for the Denver, CO group
+* retrieve all post titles from the first page
+* save the titles to a file called 'denver_posts.txt'
+
+BONUS: use pagination features to retrieve all posts of all pages in the group
+       and save them to the file
+
+'''
---
+++ week_04/web_scraping/05_bonus_task.py
@@ -0,0 +1,23 @@
+'''
+* BONUS TASK *
+* DISCLAIMER: THIS IS PROBABLY HARD AND REQUIRES SOME TWEAKING *
+
+Explore whether you can use the JavaScript support with requests_html,
+to scrape the youtube comments from a video page you are interested in.
+( you can use: https://www.youtube.com/watch?v=M54UFvJqQ5I )
+
+Parse the content, locate the usernames of the people who commented,
+and save all comments to file in the following form:
+
+username:
+    comment text
+
+username:
+    comment text
+
+etc.
+
+If requests_html doesn't quite make it and you want to learn more
+about scraping dynamic page content, check out 'selenium'.
+
+'''
