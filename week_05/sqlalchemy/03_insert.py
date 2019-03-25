75a44df1a768212462d84a9b775123c71c83e043
ArnoBali <akoldewee@gmail.com>
2019-03-25 09:28:41 +0800

    update  lab

--- week_05/sqlalchemy/03_insert.py
+++ week_05/sqlalchemy/03_insert.py
@@ -4,7 +4,6 @@ Insert a new record in the film table.
 '''
 
 import sqlalchemy as sqa
-from pprint import pprint
 
 DATABASE_URI = f'postgres+psycopg2://ak@localhost:5432/dvdrental'
 
@@ -12,7 +11,6 @@ engine = sqa.create_engine(DATABASE_URI)
 connection = engine.connect()
 metadata = sqa.MetaData()
 
-actor = sqa.Table('actor', metadata, autoload=True, autoload_with=engine)
 film = sqa.Table('film', metadata, autoload=True, autoload_with=engine)
 
 query = sqa.insert(film).values(title='het zakmes', description="Het Zakmes is een Nederlandse kinderfilm uit 1992, geregisseerd door Ben Sombogaart. Het is gebaseerd op het boek van Sjoerd Kuyper. De hoofdrollen zijn voor Olivier Tuinier, Verno Romney, Genio de Groot en Adelheid Roosen. In 2018 is de film helemaal opnieuw ingekleurd en komt deze opnieuw in de bioscoop.", language_id=1,release_year=1992, length=90)
--- week_05/sqlalchemy/04_update.py
+++ week_05/sqlalchemy/04_update.py
@@ -1,5 +1,22 @@
 '''
 Update all films in the film table to a rental_duration value of 10,
-in the length of the movie is more than 150.
+if the length of the movie is more than 150.
 
 '''
+
+
+import sqlalchemy as sqa
+
+DATABASE_URI = f'postgres+psycopg2://ak@localhost:5432/dvdrental'
+
+engine = sqa.create_engine(DATABASE_URI)
+connection = engine.connect()
+metadata = sqa.MetaData()
+
+film = sqa.Table('film', metadata, autoload=True, autoload_with=engine)
+
+query = sqa.update(film).values(rental_duration=10).where(film.columns.length > 150)
+# query = query.where(film.columns.Id == 1)
+results = connection.execute(query)
+
+result = connection.execute(query)
